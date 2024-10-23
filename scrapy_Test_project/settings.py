c31d14995e4891c7df255e4df6869f9e817829e7
haoyangjunjun <126179373+haoyangjunjun@users.noreply.github.com>
2024-10-23 23:20:20 +0800

    更新url构造方式

--- scrapy_Test_project/settings.py
+++ scrapy_Test_project/settings.py
@@ -17,16 +17,16 @@ NEWSPIDER_MODULE = "scrapy_Test_project.spiders"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0"
 
 # Obey robots.txt rules爬虫协议
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)并发请求
-CONCURRENT_REQUESTS = 8
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
-RANDOMIZE_DOWNLOAD_DELAY = True  # 随机化延迟
+DOWNLOAD_DELAY = 1
+# RANDOMIZE_DOWNLOAD_DELAY = True  # 随机化延迟
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- scrapy_Test_project/spiders/douban.py
+++ scrapy_Test_project/spiders/douban.py
@@ -1,5 +1,5 @@
 import scrapy
-from scrapy import Selector
+from scrapy import Selector, Request
 from scrapy.http import HtmlResponse
 
 from scrapy_Test_project.items import MovieItem
@@ -8,7 +8,13 @@ from scrapy_Test_project.items import MovieItem
 class DoubanSpider(scrapy.Spider):
     name = "douban"
     allowed_domains = ["movie.douban.com"]
-    start_urls = ["https://movie.douban.com/top250"]
+
+    # start_urls = ["https://movie.douban.com/top250"]
+
+    def start_requests(self):
+        # 直接构造url
+        for i in range(10):
+            yield Request(url=f'https://movie.douban.com/top250?start={i * 25}&filter=')
 
     def parse(self, response: HtmlResponse):
         sel = Selector(response)  # 选择器对象
@@ -21,7 +27,8 @@ class DoubanSpider(scrapy.Spider):
             # extract_first()这个方法用于从选择器返回的结果中提取第一个匹配项的文本内容。如果选择器没有找到任何匹配项，它将返回 None
             yield movie_item  # 告诉 Scrapy 框架：“我找到了一个我感兴趣的数据项，请将其发送到后续的管道（Pipeline）中进行处理。”
         # 用return只能返回一个
-        href_list = sel.css('div.paginator > a::attr(href)')  # 翻页选择器列表
-        for href in href_list:
-            url = response.urljoin(href.extract())  # 拼接url
-            print(url)
+        # 解析url的方法👇
+        # href_list = sel.css('div.paginator > a::attr(href)')  # 翻页选择器列表
+        # for href in href_list:
+        #     url = response.urljoin(href.extract())  # 拼接url
+        #     yield Request(url=url)
